3008b53a0c5010ee471dc0ba90d46d28b73c5b16
Navid <yaa110@gmail.com>
2016-01-29 18:28:39 +0330

    add ptime package

---
+++ ptime/ptime.go
@@ -0,0 +1,7 @@
+// In the name of Allah
+
+// Copyright (c) 2015 Navid Fathollahzade
+// This source code is licensed under MIT license that can be found in the LICENSE file.
+
+// Package ptime provides functionality for implementation of Persian (Jalali) Calendar.
+package ptime
